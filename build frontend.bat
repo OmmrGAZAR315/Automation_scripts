b7301ce58de0c4b17cc21ef7052a32bf77efb8b0
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2024-12-17 07:17:41 +0200

    refactor frontend build script to improve command execution and add success messages

--- build frontend.bat
+++ build frontend.bat
@@ -18,8 +18,9 @@ set SERVER_IP="dev.learnovia.com"
 ===============================================================================
 
 ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && git fetch --all --prune && git stash push -m \"Checkout script branch $(git rev-parse --abbrev-ref HEAD)\" && git checkout %BRANCH_NAME% && git reset --hard origin/%BRANCH_NAME%"
-ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && sudo rm -rf dist/learnoviaFront && npm run build sudo rm -rf dist/learnovia_$(date \"+%%d-%%m-%%Y\")"
-ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && mv dist/learnovia dist/learnovia_$(date \"+%%d-%%m-%%Y\") && mv dist/learnoviaFront dist/learnovia"
-
+ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && sudo rm -rf dist/learnoviaFront; npm run build; sudo rm -rf dist/learnovia_$(date \"+%%d-%%m-%%Y\")"
+echo "Frontend build completed successfully...rm old dist that match today date"
+ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && mv dist/learnovia dist/learnovia_$(date \"+%%d-%%m-%%Y\"); mv dist/learnoviaFront dist/learnovia"
+echo "renamed build successfully"
 pause
 endlocal
