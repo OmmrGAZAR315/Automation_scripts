72677f06a85fa7deb02be569591b5c7c4db2c867
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2025-01-04 18:13:51 +0200

    refactor switch DB.bat to enhance user prompts and improve default values for configuration inputs

--- switch DB.bat
+++ switch DB.bat
@@ -3,38 +3,42 @@ REM Enable delayed variable expansion
 setlocal enabledelayedexpansion
 
 :: Prompt for front destination
-if "%~1"=="" (
-    set /p "PROJECT_PATH=Please enter the front project path [SCHOOL_NAME]: "
+set /p "PROJECT_PATH=Please enter the front project path [SCHOOL_NAME]: "
+
+:: prompt for worktree
+set /p "IS_WORKTREE_EXISTS=Is this a worktree? [y/n]: "
+if "!IS_WORKTREE_EXISTS!"=="" set "IS_WORKTREE_EXISTS=n"
+
+if "!IS_WORKTREE_EXISTS!" == "y" (
+    set /p "BRANCH_NAME=Please enter the Branch name: "
+    if "!BRANCH_NAME!"=="" (
+        echo "Branch name is required"
+        pause
+        exit
+    )
+    set "PROJECT_PATH=/schools/%PROJECT_PATH%/worktrees/!BRANCH_NAME!"
 ) else (
-    set "PROJECT_PATH=%~2"
+    :: Append /dist to the upload destination
+    set "PROJECT_PATH=/schools/%PROJECT_PATH%/learnovia-backend"
 )
 
-:: Append /dist to the upload destination
-set "PROJECT_PATH=/schools/%PROJECT_PATH%/learnovia-backend"
+:: prompt for config
+set /p "CUSTOM=DO YOU WANT TO USE CONFIG? [y/n]: "
+if "!CUSTOM!"=="" set "CUSTOM=n"
 
-set "DB_PASS=%~2"
-if not defined DB_PASS (
+if /I "!CUSTOM!" == "y" (
     set /p "DB_PASS=Please enter db password: "
-    if "!DB_PASS!"=="" set "DB_PASS=Learnovia_2025*modern2025"
-)
 
-set "PEM_FILE=%~3"
-if not defined PEM_FILE (
     set /p "PEM_FILE=Please enter the PEM file path: "
-    if "!PEM_FILE!"=="" set "PEM_FILE=D:\learnovia.pem"
-)
 
-REM Set SERVER_USER from the 4th argument
-set "SERVER_USER=%~4"
-if not defined SERVER_USER (
+    REM Set SERVER_USER from the 4th argument
     set /p "SERVER_USER=Please enter the server username [default: ubuntu]: "
-    if "!SERVER_USER!"=="" set "SERVER_USER=ubuntu"
-)
 
-REM Set SERVER_IP from the 5th argument
-set "SERVER_IP=%~5"
-if not defined SERVER_IP (
     set /p "SERVER_IP=Please enter the server IP [default: dev]: "
+) else (
+    if "!DB_PASS!"=="" set "DB_PASS=Learnovia_2025*modern2025"
+    if "!PEM_FILE!"=="" set "PEM_FILE=D:\learnovia.pem"
+    if "!SERVER_USER!"=="" set "SERVER_USER=ubuntu"
     if "!SERVER_IP!"=="" set "SERVER_IP=dev"
 )
 
