9f3f8e1c034b0545d71ba2b702d9720333f279ad
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2025-01-04 18:17:58 +0200

    refactor checkout branch.bat to streamline user prompts and enhance default values for configuration inputs

--- checkout branch.bat
+++ checkout branch.bat
@@ -3,49 +3,39 @@ REM Enable delayed variable expansion
 setlocal enabledelayedexpansion
 
 :: Prompt for front destination
-if "%~1"=="" (
-    set /p "BRANCH_NAME=Please enter the Branch name: "
-) else (
-    set "BRANCH_NAME=%~1"
-)
-
+set /p "BRANCH_NAME=Please enter the Branch name: "
 
 :: Prompt for front destination
-if "%~2"=="" (
-    set /p "PROJECT_PATH=Please enter the front project path [SCHOOL_NAME]: "
-) else (
-    set "PROJECT_PATH=%~2"
-)
+set /p "PROJECT_PATH=Please enter the front project path [SCHOOL_NAME]: "
 
-:: Append /dist to the upload destination
-set "PROJECT_PATH=/schools/%PROJECT_PATH%/learnovia-backend"
+::promt for stash pop
+set /p "STASH_POP=Do you want to stash pop? [y/n]: "
+if "!STASH_POP!"=="" set "STASH_POP=n"
 
 
-set "PEM_FILE=%~3"
-if not defined PEM_FILE (
+:: prompt for config
+set /p "CUSTOM=DO YOU WANT TO USE CONFIG? [y/n]: "
+if "!CUSTOM!"=="" set "CUSTOM=n"
+
+if /I "!CUSTOM!" == "y" (
     set /p "PEM_FILE=Please enter the PEM file path: "
-    if "!PEM_FILE!"=="" set "PEM_FILE=D:\learnovia.pem"
-)
-
-REM Set SERVER_USER from the 4th argument
-set "SERVER_USER=%~4"
-if not defined SERVER_USER (
     set /p "SERVER_USER=Please enter the server username [default: ubuntu]: "
-    if "!SERVER_USER!"=="" set "SERVER_USER=ubuntu"
-)
-
-REM Set SERVER_IP from the 5th argument
-set "SERVER_IP=%~5"
-if not defined SERVER_IP (
     set /p "SERVER_IP=Please enter the server IP [default: dev]: "
+) else (
+    if "!PEM_FILE!"=="" set "PEM_FILE=D:\learnovia.pem"
+    if "!SERVER_USER!"=="" set "SERVER_USER=ubuntu"
     if "!SERVER_IP!"=="" set "SERVER_IP=dev"
 )
+:: Append /dist to the upload destination
+set "PROJECT_PATH=/schools/%PROJECT_PATH%/learnovia-backend"
 set "SERVER_IP=%SERVER_IP%.learnovia.com"
-
 ===============================================================================
 
 ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && git fetch --all --prune && git stash push -m \"Checkout script branch $(git rev-parse --abbrev-ref HEAD)\" && git checkout %BRANCH_NAME% && git reset --hard origin/%BRANCH_NAME%"
-	
+
+if "!STASH_POP!"=="y" (
+    ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && git stash pop"
+)	
 
 pause
 endlocal
