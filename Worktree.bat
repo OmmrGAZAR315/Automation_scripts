a9e881b8c7b970a59aef6a0fbf9dc3105fa2d3c7
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2025-01-24 20:13:12 +0200

    refactor Worktree.bat to improve user prompts for removing worktrees; added option to specify branch name or remove all worktrees

--- Worktree.bat
+++ Worktree.bat
@@ -10,19 +10,19 @@ setlocal enabledelayedexpansion
 
 set "PROJECT_PATH=/schools/%PROJECT_PATH%/learnovia-backend"
 
+set /p "REMOVE_WORKTEE=Do you want to remove the worktree? [y/n] [default: n]: "
+if "!REMOVE_WORKTEE!"=="" set "REMOVE_WORKTEE=n"
+
 :: prompt for config
 set /p "CUSTOM=DO YOU WANT TO USE CONFIG? [y/n]: "
 if "!CUSTOM!"=="" set "CUSTOM=n"
 
 if /I "!CUSTOM!" == "y" (
-
-    set /p "REMOVE_WORKTEE=Do you want to remove the worktree? [y/n] [default: n]: "
     set /p "PEM_FILE=Please enter the PEM file path: "
     set /p "SERVER_USER=Please enter the server username [default: ubuntu]: "
     set /p "SERVER_IP=Please enter the server IP [default: dev]: "
 
 ) else (
-
     if "!REMOVE_WORKTEE!"=="" set "REMOVE_WORKTEE=n"
     if "!PEM_FILE!"=="" set "PEM_FILE=D:\learnovia.pem"
     if "!SERVER_USER!"=="" set "SERVER_USER=ubuntu"
@@ -33,9 +33,20 @@ set "SERVER_IP=%SERVER_IP%.learnovia.com"
 ===============================================================================
 
 if "%REMOVE_WORKTEE%"=="y" (
-    ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && sudo git worktree remove %BRANCH_NAME% --force && sudo rm -rf %PROJECT_PATH%/../worktrees/%BRANCH_NAME%"
-    echo Removed worktree "%BRANCH_NAME%" from %PROJECT_PATH%
-    pause
+    ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "ls %PROJECT_PATH%/../worktrees/"
+    set /p "DIR_NAME=Please enter the branch name you want to remove: "
+    if not "!DIR_NAME!"=="" (
+        if "!DIR_NAME!"=="all" (
+            ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "sudo rm -rf %PROJECT_PATH%/../worktrees/*"
+            echo Removed all worktrees from %PROJECT_PATH%../worktrees/
+        ) else (
+            ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "sudo rm -rf %PROJECT_PATH%/../worktrees/!DIR_NAME!"
+        )
+    ) else (
+        ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && sudo git worktree remove %BRANCH_NAME% --force; sudo rm -rf %PROJECT_PATH%/../worktrees/%BRANCH_NAME%"
+        echo Removed worktree "%BRANCH_NAME%" from %PROJECT_PATH%
+        pause
+    )
     ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && git worktree list"
     pause
     exit /b 0
