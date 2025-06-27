b40cabd59913d1dce2010cef306d30ebf1295c21
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2025-06-28 00:20:50 +0300

    refactor switch DB.bat to enhance database management prompts and streamline project path handling

--- switch DB.bat
+++ switch DB.bat
@@ -2,9 +2,10 @@
 REM Enable delayed variable expansion
 setlocal enabledelayedexpansion
 
-echo dev mob front shahadat
+echo dev mob front shahadat new
 :: Prompt for front destination
 set /p "PROJECT_PATH=Please enter the front project path [SCHOOL_NAME]: "
+set /p "DROP_DB=Do you want to drop the database? [y/n]: "
 
 :: prompt for worktree
 set /p "IS_WORKTREE_EXISTS=Is this a worktree? [y/n]: "
@@ -20,7 +21,7 @@ if "!IS_WORKTREE_EXISTS!" == "y" (
     set "PROJECT_PATH=/schools/%PROJECT_PATH%/worktrees/!BRANCH_NAME!"
 ) else (
     :: Append /dist to the upload destination
-    set "PROJECT_PATH=/schools/%PROJECT_PATH%/learnovia-backend"
+    set "PROJECT_PATH=/schools/%PROJECT_PATH%/backend"
 )
 
 :: prompt for config
@@ -38,22 +39,22 @@ if "!SERVER_IP!"=="" set "SERVER_IP=dev"
 if "!DB_PASS!"=="" set "DB_PASS=Learnovia_2025*modern2025"
 if "!PEM_FILE!"=="" set "PEM_FILE=D:\learnovia.pem"
 if "!SERVER_USER!"=="" set "SERVER_USER=ubuntu"
+if "!DROP_DB!"=="" set "DROP_DB=n"
 
 set "SERVER_IP=%SERVER_IP%.learnovia.com"
-set "domains=dev mob front shahadat"
+set "domains=dev mob front shahadat new"
 ===============================================================================
-
-ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "mysql -u root -p%DB_PASS% -e 'SHOW DATABASES;'"
 echo.
-set /p "DROP_DB=Do you want to drop the database? [y/n]: "
+ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "mysql -u root -p%DB_PASS% -e 'SHOW DATABASES;'" | findstr /V "information_schema" | findstr /V "mysql" | findstr /V "sys" | findstr /V "performance_schema"
+echo.
 if "%DROP_DB%" == "y" (
-   
 
+:drop_db
 for %%D in (%domains%) do (
-    set "NEW_PROJECT_PATH=/schools/%%D/learnovia-backend"
+    set "NEW_PROJECT_PATH=/schools/%%D/backend"
     echo %%D
     echo.
-    ssh -i %PEM_FILE% -o StrictHostKeyChecking=no %SERVER_USER%@%SERVER_IP% "sudo grep -Po '^DB_DATABASE=\K.*' !NEW_PROJECT_PATH!/.env"
+    ssh -i %PEM_FILE% -o StrictHostKeyChecking=no %SERVER_USER%@%SERVER_IP% "sudo grep -Po '^MYSQL_DATABASE=\K.*' !NEW_PROJECT_PATH!/.env"
 )
      set /p "DB_NAME=Please enter the database name: "
 
@@ -67,10 +68,24 @@ for %%D in (%domains%) do (
         ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "mysql -u root -p%DB_PASS% -e 'DROP DATABASE !DB_NAME!;'"
         echo Database dropped successfully
     )
+
+    set /p "DROP_DB=Do you want to drop another database? [y/n]: "
+    if "!DROP_DB!"=="" set "DROP_DB=n"
+    if "!DROP_DB!" == "y" (
+        ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "mysql -u root -p%DB_PASS% -e 'SHOW DATABASES;'" | findstr /V "information_schema" | findstr /V "mysql"
+        echo.
+        goto :drop_db
+    ) else (
+        echo.
+        echo.
+        echo Database drop process completed
+        echo.
+        echo.
+    )
 )
 
 echo server databse selected is:
-ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "sudo grep -Po '^DB_DATABASE=\K.*' %PROJECT_PATH%/.env"
+ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "sudo grep -Po '^MYSQL_DATABASE=\K.*' %PROJECT_PATH%/.env"
 
 
 echo.
@@ -87,13 +102,34 @@ if "%DB%"=="" (
     pause
     exit
 )
-ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% " sudo sed -i 's/^DB_DATABASE=.*/DB_DATABASE=%DB%/' %PROJECT_PATH%/.env "
+ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% " sudo sed -i 's/^MYSQL_DATABASE=.*/MYSQL_DATABASE=%DB%/' %PROJECT_PATH%/.env "
 
 ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && php artisan optimize:clear"
 
 echo Database name changed successfully
 
-ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "sudo grep -Po '^DB_DATABASE=\K.*' %PROJECT_PATH%/.env"
+ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "sudo grep -Po '^MYSQL_DATABASE=\K.*' %PROJECT_PATH%/.env"
+
+
+set /p "RUN_MIGRATIONS=Do you want to run migrations? [y/n]: "
+if "!RUN_MIGRATIONS!"=="" set "RUN_MIGRATIONS=n"
+echo.
+if /i "!RUN_MIGRATIONS!" == "y" (
+    echo Running migrations...
+    ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && php artisan migrate --no-interaction"
+    echo Seeding database...
+    ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && php artisan db:seed --class=TranslationSeeder"
+    ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && php artisan db:seed --class=PermissionSeeder"
+    echo Done migrating and seeding database.
+    @REM ssh -i %PEM_FILE% %SERVER_USER%@%SERVER_IP% "cd %PROJECT_PATH% && php artisan db:seed --class=PermTranSeeders"    
+)
+
+:: set /p "RUN_MIGRATIONS=Do you want to run migrations? [y/n]: "
+:: if "!RUN_MIGRATIONS!"=="" set "RUN_MIGRATIONS=n"
+:: if /i "!RUN_MIGRATIONS!" == "y" (
+::     echo Running migrations...
+:: )
+
 
 pause
 endlocal
